412954b646c9088360826ec6e980c3fdabd17eee
kuehar <green95251@gmail.com>
2023-10-14 15:00:33 +0900

    Initial Commit

---
+++ OSLogStore/OSLogStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSLogStoreApp.swift
+//  OSLogStore
+//
+//  Created by kuehar on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct OSLogStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
