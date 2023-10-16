357986b0cac0b56cb0dbc49ec688d860739680e5
kuehar <green95251@gmail.com>
2023-10-17 02:21:46 +0900

    Fixed Files.

--- OSLogStore/ContentView.swift
+++ OSLogStore/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import OSLog
 
 struct ContentView: View {
+    @State private var messages:String = ""
+    @State private var isLogSendAlertDisplay = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section(header: Text("報告")) {
+                Button(action: {
+                    exportToLogFile()
+                    isLogSendAlertDisplay = true
+                }, label: {
+                    Text("アプリがクラッシュしたことを報告する")
+                })
+                .alert("通知", isPresented:$isLogSendAlertDisplay) {
+                    Button("OK") {
+                        // ログ送付処理をここに書く
+                        isLogSendAlertDisplay = false
+                    }
+                    Button("Cancel",role:.cancel){}
+                } message: {
+                    Text("クラッシュしたことを開発者に報告します。よろしいですか？")
+                }
+            }
         }
-        .padding()
+    }
+    func exportToLogFile() {
+        do {
+            let store = try OSLogStore(scope: .currentProcessIdentifier)
+            let position = store.position(date: Date().addingTimeInterval(-3600))
+            let enumerator = try store.__entriesEnumerator(options: [.reverse], position: position, predicate: nil)
+            
+            var logString = ""
+            messages = ""
+            enumerator.forEach { element in
+                if let message = (element as? OSLogEntry)?.composedMessage {
+                    logString += message + "\n"
+                    messages += message + "\n"
+                }
+            }
+            
+            // ドキュメントディレクトリのパスを取得
+            if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let logURL = documentDirectory.appendingPathComponent("logs.txt")
+                try logString.write(to: logURL, atomically: true, encoding: .utf8)
+                print("Logs exported to: \(logURL)")
+            }
+        } catch {
+            print("Error exporting logs: \(error)")
+        }
     }
 }
 
